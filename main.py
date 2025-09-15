ffb80d29cb2154b02cff3e595ad662ba8a44cc57
Wangcb_Study <wangcbstudy@gmail.com>
2025-09-15 16:14:06 +0800

    手動寫第1個Flask檔案

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# todo: 請建立一個 Flask 網站，並在首頁顯示 "Hello, Flask!"。
-
 from flask import Flask
-app = Flask(__name__)   
+app = Flask(__name__)
+
 @app.route('/')
-def hello():
-    return "Hello, Flask!"
-if __name__ == '__main__':  
+def index():
+        return '<h1 style="COLOR:green">Hello !! 你好 !!</h1>'
+
+if __name__ == "__main__":
     app.run(debug=True)
---
+++ main_ai.py
@@ -0,0 +1,18 @@
+# todo: 請建立一個 Flask 網站，並在首頁顯示 "Hello, Flask!"。
+
+from flask import Flask
+app = Flask(__name__)   
+@app.route('/')
+def hello():
+    return "Hello, Flask!"
+if __name__ == '__main__':  
+    app.run(debug=True)
+
+###################
+# save this as app.py
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello():
+#     return "Hello, World!"
